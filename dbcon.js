da0399e92bc76cd2f01534eb3031f97dd6f6ebd8
Hyunwook Shin <46451881+shinhyu3@users.noreply.github.com>
2019-04-27 16:41:05 -0700

    Delete dbcon.js

--- dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_saundema',
-  password        : '9862',
-  database        : 'cs340_saundema'
-});
-module.exports.pool = pool;
